864a20fcb069e0763fbc5b0f7a5bee38c8e110ba
morokoshi <bleing@hotmail.co.jp>
2019-01-31 01:25:33 +0900

    20190131 - フィルターの内容見直し - 設定ファイル機能の追加

--- Cleanup-WsusContents/Cleanup-WsusContents/Cleanup-WsusContents.ps1
+++ Cleanup-WsusContents/Cleanup-WsusContents/Cleanup-WsusContents.ps1
@@ -1,22 +1,29 @@
 ﻿#Requires -Version 4.0
 #Requires -RunAsAdministrator
+$Host.PrivateData.VerboseForegroundColor = "Cyan"
 #
-# 20181027 WSUS から不要な更新プログラムを拒否する
+# 20190130 WSUS から不要な更新プログラムを拒否する
 #
 # このスクリプトは現状ベースで作成されたものです。今後の更新プログラムに対応するには、直接WSUSかスクリプトのメンテナンスが必要になることを理解してください。
 # このスクリプトを利用したことによる問題に対する責任は一切負いません。実行する前に必ず検証をしてください。
 
-#最初に変数をメンテナンスしてください
-$FeatureUpdatesFilterFileName = "C:\Tools\Scripts\Wsus\Filter-FU-対象の定義ファイル名.txt"
-$QualityUpdatesFilterFileName = "C:\Tools\Scripts\Wsus\Filter-QU-対象の定義ファイル名.txt"
-$DummyFilePath = "C:\Tools\Scripts\Wsus\Dummy.tmp"
-$DummyFileSize = 4294967296
-$WsusDBMaintenanceScriptPath = "C:\Tools\Scripts\Wsus\Scripts-WsusDBMaintenance.sql"
+#最初に Settings.Current.json をメンテナンスしてください
+If (!(Test-Path "Settings.Current.json")){
+	Copy-Item "Assets\Settings.Default.json" "Settings.Current.json"
+}
+$Settings = Get-Content "Settings.Current.json" -Encoding UTF8 -Raw | ConvertFrom-Json
 
-$SqlCmdPath = "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE"
-$SqlServerPath = "np:\\.\pipe\Microsoft##WID\tsql\query"
-$Host.PrivateData.VerboseForegroundColor = "Cyan"
-$Wsus = Get-WsusServer -Name localhost -PortNumber 8530
+#Settings.Current.json をパースします
+$FeatureUpdatesFilterFileNames = $Settings.DeclineRule.FeatureUpdatesFilter.FileNames
+$QualityUpdatesFilterFileNames = $Settings.DeclineRule.QualityUpdatesFilter.FileNames
+$DummyFilePath = $Settings.ReservedFile.Path
+$DummyFileSize = $Settings.ReservedFile.Size
+$WsusDBMaintenanceScriptPath = $Settings.MaintenanceSql.ScriptPath
+$SqlCmdPath = $Settings.MaintenanceSql.SqlCmdPath
+$SqlServerPath = $Settings.MaintenanceSql.ServerPath
+$IsDeclineMsOfficeUpdates = $Settings.QualityUpdatesFilter.IsDeclineMsOfficeUpdates
+$TargetMsOfficeArchitecture = $Settings.QualityUpdatesFilter.TargetMsOfficeArchitecture
+$Wsus = Get-WsusServer -Name $Settings.Wsus.Server -PortNumber $Settings.Wsus.Port
 
 
 #特定の機能更新プログラムを拒否
@@ -66,7 +73,7 @@ Function Cleanup-Wsus(){
 }
 
 
-#スクリプトが正常に動作するためのダミーファイルを削除する
+#空き領域が少なくなりがちな環境で、スクリプトが正常に動作するためのダミーファイルを削除する
 If (Test-Path $DummyFilePath){
 	Remove-Item -Path $DummyFilePath -Force | Out-Null
 }
@@ -82,26 +89,30 @@ Decline-QualityUpdates($FilteredUpdates)
 Write-Host ("** 機能更新プログラムを拒否")
 $FilteredUpdates = @()
 $AllUpdates = $Wsus.GetUpdates() | Where-Object {$_.IsDeclined -eq $False -and $_.UpdateClassificationTitle -eq "Upgrades"}
-$FeatureUpdatesFilters = Get-Content -Path $FeatureUpdatesFilterFileName
-$AllUpdates | ForEach-Object {
-	$UpdateInformation = $_
-	$FeatureUpdatesFilters | ForEach-Object {
-		If ($UpdateInformation.GetInstallableItems().Files.Name -like $_){
-			$FilteredUpdates += $UpdateInformation
+ForEach ($FeatureUpdatesFilterFileName in $FeatureUpdatesFilterFileNames){
+	$FeatureUpdatesFilters = Get-Content -Path "Filters\FeatureUpdates\$FeatureUpdatesFilterFileName"
+	$AllUpdates | ForEach-Object {
+		$UpdateInformation = $_
+		$FeatureUpdatesFilters | ForEach-Object {
+			If ($UpdateInformation.GetInstallableItems().Files.Name -like $_){
+				$FilteredUpdates += $UpdateInformation
+			}
 		}
 	}
+	Decline-FeatureUpdates($FilteredUpdates)
 }
-Decline-FeatureUpdates($FilteredUpdates)
-
 
 Write-Host "** 品質更新プログラムを拒否"
 $FilteredUpdates = @()
 $AllUpdates = $Wsus.GetUpdates() | Where-Object IsDeclined -eq $False
-#Officeの更新プログラムのメンテナンス
-#Office 2016 64ビット版 の更新プログラムをすべて拒否
-# $FilteredUpdates += $AllUpdates | Where-Object {$_.Title -like "*64 ビット版*" -and $_.Title -notlike "*32 ビット版*" -and $_.ProductTitles -eq "Office 2016"}
-Get-Content -Path $QualityUpdatesFilterFileName | ForEach-Object {
-    $FilteredUpdates += $AllUpdates | Where-Object LegacyName -like $_
+ForEach ($QualityUpdatesFilterFileName in $QualityUpdatesFilterFileNames){
+	Get-Content -Path "Filters\QualityUpdates\$QualityUpdatesFilterFileName" | ForEach-Object {
+		$FilteredUpdates += $AllUpdates | Where-Object LegacyName -like $_
+	}
+}
+#Officeの更新プログラム
+If ($IsDeclineMsOfficeUpdates){
+	$FilteredUpdates += $AllUpdates | Where-Object {$_.Title -like "*$TargetMsOfficeArchitecture*" -and $_.ProductTitles -like "Office *"}
 }
 Decline-QualityUpdates($FilteredUpdates)
 
@@ -116,5 +127,5 @@ Start $SqlCmdPath ("-S", $SqlServerPath, "-i", $WsusDBMaintenanceScriptPath)
 #$Wsus.GetUpdates() | Where-Object {$_.IsDeclined -eq $False -and $_.IsApproved -eq $False} | Select Title, ProductTitles, CreationDate, LegacyName | Out-GridView -Title "未承認の更新プログラム"
 #$Wsus.GetUpdates() | Where-Object {$_.IsDeclined -eq $False -and $_.IsApproved -eq $False} | Export-Csv 未承認の更新プログラム.csv -Encoding UTF8
 
-#スクリプトが正常に動作するために、作業用のダミーファイルを作成する
+#空き領域が少なくなりがちな環境で、スクリプトが正常に動作するためのダミーファイルを作成する
 FsUtil File CreateNew $DummyFilePath $DummyFileSize | Out-Null
